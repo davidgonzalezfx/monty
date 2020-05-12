6ef1a9881a68248da7c4420466edbe662bdae797
fernandoSandoval <sandovbarr@gmail.com>
2020-05-12 11:27:43 -0500

    [ADD]conditions for argc and openfile

--- main.c
+++ main.c
@@ -7,7 +7,18 @@ int main (int ac, char **av)
 	char* readed = NULL;
 	size_t len = 0;
 
+	if (argc != 2)
+	{
+		dprintf(STDOUT_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
 	monty_file = fopen(av[1], "r");
+	if (!monty_file)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
 	return (0);
 }
