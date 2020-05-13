3b9fe631848b0536815088f1dc0ac4fe197bb1ff
fernandoSandoval <sandovbarr@gmail.com>
2020-05-13 15:15:02 -0500

    Fix - Breakline in error message

--- stack_core_funs.c
+++ stack_core_funs.c
@@ -21,7 +21,7 @@ void pint(stack_t **stack, unsigned int line_number)
 
 	if (!(*stack))
 	{
-		printf("L%d: can't pint, stack empty", initial.line_cnt);
+		printf("L%d: can't pint, stack empty\n", initial.line_cnt);
 		free_dlistint(initial.head);
 		free(initial.readed);
 		fclose(initial.monty_file);
