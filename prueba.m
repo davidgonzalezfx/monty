ea6564cfabe550778fac6eee1ff0ad85175f72a7
davidgonzalezfx <dvdramos16@gmail.com>
2020-05-13 18:44:36 -0500

    Fix - Loop condition in pstr function

--- prueba.m
+++ prueba.m
@@ -7,7 +7,7 @@ push 98
 push 108
 push 111
 push 72
-push 0
+push 32
 push 110
 push 111
 push 116
--- stack_aux2_funs.c
+++ stack_aux2_funs.c
@@ -52,7 +52,7 @@ void pstr(stack_t **stack, unsigned int line_number)
 	}
 	while (aux->next)
 		aux = aux->next;
-	while (aux->prev)
+	while (aux)
 	{
 		if (isascii(aux->n) == 0 || aux->n == 0)
 			break;
